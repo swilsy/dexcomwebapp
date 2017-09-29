11cbe30a1af83430e0473e6d699a5c18ccee09ad
Kenny Krosky <kennykrosky@Kennys-MacBook-Pro.local>
2017-09-28 17:10:30 -0700

    updates

--- index.php
+++ index.php
@@ -2,9 +2,34 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="https://use.fontawesome.com/a9ce52f234.js"></script>
 </head>
 <body>
-	<a href="https://api.dexcom.com/v1/oauth2/login?client_id=XE15nFWB4ACRnGY7JwUEcJG1Lizhhzxm&redirect_uri=http://localhost:3000/dexcomapi/home.php&response_type=code&scope=offline_access">Login</a>
+
+	<div class="container">
+		<figure class="the-logo">
+			<img src="assets/img/logo.png" alt="">
+		</figure>
+
+
+		<a class="start" href="https://api.dexcom.com/v1/oauth2/login?client_id=XE15nFWB4ACRnGY7JwUEcJG1Lizhhzxm&redirect_uri=http://localhost:3000/dexcomapi/home.php&response_type=code&scope=offline_access">Login</a>
+
+		<div class="the-what">
+			<p>Get 24 hour readings/graph/average</p>
+		</div>
+
+		<div>
+			<small>All assets/data belong to <a href="">Dexcom</a> This application is not to be used for medical treatment. By clicking "login" you are agreeing that this site or the owner of this site is not reponsible for any medical reponsibilities</small>
+		</div>
+
+
+		<div class="the-what">
+			<small>by kenny krosky <i class="fa fa-twitter" aria-hidden="true"></i></small>
+		</div>
+
+		
 	
+	</div>
 </body>
 </html>
--- view.php
+++ view.php
@@ -7,11 +7,13 @@
 	<title>Document</title>
 		<script src="assets/js/jquery.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
+		<link rel="stylesheet" href="assets/css/style.css">
+	<script src="https://use.fontawesome.com/a9ce52f234.js"></script>
 </head>
 <body>
 
-	<div style="width: 900px; height: 400px; margin: auto;">
-		<canvas id="myChart" width="200" height="200"></canvas>
+	<div class="graph">
+		<canvas id="myChart" height="200"></canvas>
 	</div>
 
 
