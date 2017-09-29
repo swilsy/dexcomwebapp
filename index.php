0f52d826789b6475d69a0a6ee01dd2a35cdc6032
Kenny Krosky <kennykrosky@Kennys-iMac.local>
2017-09-29 10:13:39 -0700

    small updates

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Dexcom Readings For Us</title>
 	<link rel="stylesheet" href="assets/css/style.css">
 	<script src="https://use.fontawesome.com/a9ce52f234.js"></script>
 	<meta name=viewport content="width=device-width, initial-scale=1">
+	<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107286439-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-107286439-1');
+</script>
+
 </head>
 <body>
 
